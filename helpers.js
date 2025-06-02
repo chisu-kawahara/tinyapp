e37d8120efcdd4839399b75a3cdf3f429d48dc0d
chisu-kawahara <chisu.kawahara@gmail.com>
2025-06-01 21:51:54 -0700

    Test helper functions

--- helpers.js
+++ helpers.js
@@ -1,3 +1,8 @@
+//This function takes two inputs:
+//email – the email you want to look for (a string).
+//users – an object that stores all your user accounts (like a mini database).
+//This checks if the email of the current user matches the one we’re looking for. If it does, it returns the whole user object. 
+
 function getUserByEmail(email, users) {
   for (const userId in users) {
     const user = users[userId];
